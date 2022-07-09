ffd3268e77cc65fce972266db8516bf4cb780e2e
Dr DanG <75981442+zmraj@users.noreply.github.com>
2022-07-09 07:33:57 +0530

    Update images.py

--- configs/images.py
+++ configs/images.py
@@ -5,32 +5,32 @@ import os
 
 
 # ❌ FEEDBACK LINK ❌ [EDITABLE]
-FEEDBACK = "https://telegram.dog/ilovepdf_bot"
+FEEDBACK = "https://t.me/DeltaBotsOfficial"
 
 
 # ❌ DEFAULT THUMBNAIL ❌ [EDITABLE]
 # NB: Thumbnails can’t be reused and can be only uploaded as a new file.
 # from Configs.images import PDF_THUMBNAIL
 PDF_THUMBNAIL = "./images/thumbnail.jpeg"
-# PDF_THUMBNAIL="https://te.legra.ph/file/a780afd8b5cef866a388d.jpg"
+# PDF_THUMBNAIL="https://telegra.ph/file/3d8a2ac0145fc87b432b0.jpg"
 
 
 # ❌ WELCOME IMAGE ❌ [EDITABLE]
 # from Configs.images import WELCOME_PIC
 # WELCOME_IMAGE="./images/start.jpeg"
-WELCOME_PIC = "https://te.legra.ph/file/8dfa3760df91a218a629c.jpg"
+WELCOME_PIC = "https://telegra.ph/file/3d8a2ac0145fc87b432b0.jpg"
 
 
 # ❌ BANNED IMAGE ❌ [EDITABLE]
 # from Configs.images import BANNED_PIC
 # BANNED_MESSAGE="./images/banned.jpeg"
-BANNED_PIC = "https://te.legra.ph/file/8dfa3760df91a218a629c.jpg"
+BANNED_PIC = "https://telegra.ph/file/3d8a2ac0145fc87b432b0.jpg"
 
 
 # ❌ BIG FILE ❌ [EDITABLE]
 # from Configs.images import BIG_FILE
 #  = "./images/bigFile.jpeg"
-BIG_FILE = "https://te.legra.ph/file/8dfa3760df91a218a629c.jpg"
+BIG_FILE = "hhttps://telegra.ph/file/3d8a2ac0145fc87b432b0.jpg"
 
 
 # ❌ Load UsersId with custom thumbnail ❌
@@ -41,4 +41,4 @@ CUSTOM_THUMBNAIL_U, CUSTOM_THUMBNAIL_C = [], []
 DEFAULT_NAME = os.environ.get("DEFAULT_NAME", False)
 
 
-#                                                             @nabilanavab
+#                                                            @DeltaBotsOfficial
